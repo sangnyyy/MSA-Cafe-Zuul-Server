f3e3ce3e0c7deee4b25f37742b986debd997ba26
SML <dltkdals2202@gmail.com>
2019-02-10 01:31:48 +0900

    feat : project added
    Signed-off-by: SML <dltkdals2202@gmail.com>

---
+++ src/main/java/me/sml/zuul/MsaZuulApplication.java
@@ -0,0 +1,14 @@
+package me.sml.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsaZuulApplication.class, args);
+    }
+
+}
+
